a802ce82eaa915007db1641e099976ec9a6c317d
Hipno <eylayvictor@gmail.com>
2025-02-21 15:46:13 +0000

    Asignatura IB

---
+++ Biseccion.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int Biseccion(numero1, numero2, valor_minimo, funcion) {
+  double errores{0}, resultado{0};
+  int m{0};
+  std::cout << "| m | a_m | b_m | x_m+1 | f(x_m+1) | Errores |"
+  do {
+    errores=(numero1+numero2)/pow(2, m);
+    std::
+    m++;
+  } while (errores < valor_minimo);
+
+
+
+}
